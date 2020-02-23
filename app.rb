7d448156670f4de6824908c1c860878ceaaa52ff
ryan m <contact@ryanmeek.com>
2020-02-23 19:36:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ class App < Sinatra::Base
   get("/hello/:name") {@user_name = params[:name]; "Hello #{@user_name}!"}
   get("/goodbye/:name") {@user_name = params[:name]; "Goodbye, #{@user_name}."}
 
-  get("/multiply/:num1/:num2")
+  get("/multiply/:num1/:num2") {"#{params[:num1].to_i * params[:num2].to_i}"}
     # @num1 = params[:num1].to_i
     # @num2 = params[:num2].to_i
     "#{params[:num1].to_i * params[:num2].to_i}"
-  end
+
 
 end
