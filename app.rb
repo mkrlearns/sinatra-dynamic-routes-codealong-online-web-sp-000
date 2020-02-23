1c5599b2bd0a11662034e855110b65242f00185e
ryan m <contact@ryanmeek.com>
2020-02-23 19:36:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ class App < Sinatra::Base
   get("/goodbye/:name") {@user_name = params[:name]; "Goodbye, #{@user_name}."}
 
   get("/multiply/:num1/:num2")
-    def p_to_i(i) params[i].to_i
     # @num1 = params[:num1].to_i
     # @num2 = params[:num2].to_i
     "#{params[:num1].to_i * params[:num2].to_i}"
